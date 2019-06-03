5b40cc3fc787086f9f6012d30f816b8d5f8a2705
Hannah Guo <hguo2102@gmail.com>
2019-06-03 13:13:30 -0400

    latest update joining groups

--- public/scripts/signin.js
+++ public/scripts/signin.js
@@ -1,26 +1,34 @@
-// var config = {
-//     apiKey: "AIzaSyBdGCZhuQPYhsQsLYhcfmrYK7ZG7pZs4fQ",
-//     authDomain: "edumapbase.firebaseapp.com",
-//     databaseURL: "https://edumapbase.firebaseio.com",
-//     projectId: "edumapbase",
-//     storageBucket: "edumapbase.appspot.com",
-//     messagingSenderId: "763874557198",
-//     appID: "1:763874557198:web:4fd77b99972e788c",
-// };
+var config = {
+    apiKey: "AIzaSyBdGCZhuQPYhsQsLYhcfmrYK7ZG7pZs4fQ",
+    authDomain: "edumapbase.firebaseapp.com",
+    databaseURL: "https://edumapbase.firebaseio.com",
+    projectId: "edumapbase",
+    storageBucket: "edumapbase.appspot.com",
+    messagingSenderId: "763874557198",
+    appID: "1:763874557198:web:4fd77b99972e788c",
+};
 
-// if (!firebase.apps.length) firebase.initializeApp(config);
+if (!firebase.apps.length) firebase.initializeApp(config);
 
-// function logIn(){
-//     var provider = new firebase.auth.GoogleAuthProvider();
-//     firebase.auth().signInWithPopup(provider).then(function(result) {
-//         var token = result.credential.accessToken;
-//         var user = result.user;
-//         console.log(user);
-//     }).catch(function(error) {
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         var email = error.email;
-//         var credential = error.credential;
-//         console.log(errorCode);
-//     });
-// }
+function logIn() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+        console.log(user.displayName);
+    }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode);
+    });
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log("Signed in!");
+    } else {
+        console.log("Not Signed in!");
+    }
+});
